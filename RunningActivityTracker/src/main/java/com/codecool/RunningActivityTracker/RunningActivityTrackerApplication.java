deaa33f3853dba829cf5c6d20e358346186dae78
Manuel <manuel.vanek@gmail.com>
2023-11-14 20:09:25 +0100

    project setup

---
+++ RunningActivityTracker/src/main/java/com/codecool/RunningActivityTracker/RunningActivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.RunningActivityTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningActivityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningActivityTrackerApplication.class, args);
+	}
+
+}
---
+++ RunningActivityTracker/src/test/java/com/codecool/RunningActivityTracker/RunningActivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.RunningActivityTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningActivityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
